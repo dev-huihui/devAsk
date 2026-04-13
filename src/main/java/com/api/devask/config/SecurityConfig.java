bb58d41c4bc74e24d613f74caa2bbd1c51cc511c
dev-huihui <hejeoung212@naver.com>
2026-04-13 15:54:14 +0900

    feat: spring security 암호화 메소드 추가  및 사용자 추가/수정시 암호화처리

--- src/main/java/com/api/devask/config/SecurityConfig.java
+++ src/main/java/com/api/devask/config/SecurityConfig.java
@@ -5,31 +5,35 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // 2026.04.13 csrf 비활성화(JWT를 사용할 것이기 때문)
         http.csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth
-                    .requestMatchers(
-                            "/login",
-                            "/signup",
-                            "/css/**",
-                            "/js/**"
-                    ).permitAll()
-                    .anyRequest().authenticated()
-            )
-            .formLogin(form -> form
-                    .loginPage("/login")
-                    .defaultSuccessUrl("/", true)
-                    .permitAll()
-            )
-            .logout(logout -> logout
-                    .logoutSuccessUrl("/login")
-            );
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/login",
+                                "/signup",
+                                "/css/**",
+                                "/js/**")
+                        .permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/", true)
+                        .permitAll())
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/login"));
 
         return http.build();
     }
--- src/main/java/com/api/devask/service/user/Impl/UserServiceImpl.java
+++ src/main/java/com/api/devask/service/user/Impl/UserServiceImpl.java
@@ -6,19 +6,24 @@ import com.api.devask.repository.user.UserRepository;
 import com.api.devask.service.user.UserService;
 import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     // 2026.01.28 user 정보를 저장하는 메소드
     @Override
     @Transactional
     public void addUser(User user) {
-        // TODO: 비밀번호 암호화 처리하도록 로직 추가 필요
-        
+        // 2026.04.13 비밀번호 암호화 처리
+        if (user.getPassword() != null) {
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+        }
+
         // 2026.01.28 user 정보 저장
         userRepository.save(user);
     }
@@ -27,11 +32,13 @@ public class UserServiceImpl implements UserService {
     @Override
     @Transactional
     public void modifyUser(User user) {
-        /* 2026.02.05 userID로 정보 찾기
-         * Optional 안에 값이 있으면 그 값을 꺼내고, 없으면 예외를 던져라 */
+        /*
+         * 2026.02.05 userID로 정보 찾기
+         * Optional 안에 값이 있으면 그 값을 꺼내고, 없으면 예외를 던져라
+         */
         User changeUser = userRepository.findById(user.getUserId())
                 .orElseThrow(() -> new IllegalArgumentException("user 없음"));
-        
+
         // 2026.02.05 user 이름 값이 있을 경우에만
         if (user.getUserName() != null) {
             changeUser.setUserName(user.getUserName());
@@ -39,9 +46,8 @@ public class UserServiceImpl implements UserService {
 
         // 2026.02.05 user 비밀번호 값이 있을 경우에만
         if (user.getPassword() != null) {
-            // TODO: 비밀번호 암호화하는 로직 추가 필요
-            
-            changeUser.setPassword(user.getPassword());
+            // 2026.04.13 비밀번호 암호화 처리
+            changeUser.setPassword(passwordEncoder.encode(user.getPassword()));
         }
 
         // 2026.02.05 user 이메일 값이 있을 경우에만
@@ -54,8 +60,10 @@ public class UserServiceImpl implements UserService {
     @Override
     @Transactional(readOnly = true)
     public UserResponseDTO loadUserByUserId(String userId) {
-        /* 2026.02.05 userID로 정보 찾기
-         * Optional 안에 값이 있으면 그 값을 꺼내고, 없으면 예외를 던져라 */
+        /*
+         * 2026.02.05 userID로 정보 찾기
+         * Optional 안에 값이 있으면 그 값을 꺼내고, 없으면 예외를 던져라
+         */
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new RuntimeException("사용자 없음"));
 
