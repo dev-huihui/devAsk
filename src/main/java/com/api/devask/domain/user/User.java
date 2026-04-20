1195445fca0b60427c59e4309e2eb568ab668878
dev-huihui <hejeoung212@naver.com>
2026-04-20 17:40:43 +0900

    feat: 로그인 관련 부가적인 기능을 위한 데이터베이스 컬럼 추가 및 default 값 설정

--- src/main/java/com/api/devask/domain/user/User.java
+++ src/main/java/com/api/devask/domain/user/User.java
@@ -29,6 +29,35 @@ public class User {
     @Column(name = "ROLE", nullable = false)
     private String role;
 
+    // 2026.04.20 부가적인 기능(로그인 실패횟수, 계정잠금)
     @Column(name = "FAIL_CNT", nullable = false)
-    private Integer failCnt;
+    private Integer failCnt = 0;
+
+    @Column(name = "ACCOUNT_LOCK", nullable = false)
+    private String accountLock = "N";
+
+    @Column(name = "USE_YN", nullable = false)
+    private String useYn = "Y";
+
+    @Column(name = "DELETE_YN", nullable = false)
+    private String deleteYn = "N";
+
+    @PrePersist
+    public void prePersist() {
+        if (this.failCnt == null) {
+            this.failCnt = 0;
+        }
+
+        if (this.accountLock == null) {
+            this.accountLock = "N";
+        }
+
+        if (this.useYn == null) {
+            this.useYn = "Y";
+        }
+
+        if (this.deleteYn == null) {
+            this.deleteYn = "N";
+        }
+    }
 }
